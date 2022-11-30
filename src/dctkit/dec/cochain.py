9d04d903f1a4cf3f5645c00420d4c95a83681b49
Simone Manti <manti998@gmail.com>
2022-11-30 16:15:15 +0100

    Created dec folder, implemented cochain class and coboundary

---
+++ src/dctkit/dec/cochain.py
@@ -0,0 +1,51 @@
+from src.dctkit.mesh import simplex
+from src.dctkit.math import spmv
+
+
+class cochain(simplex.simplicial_complex):
+    """Cochain associated to a simplicial complex
+
+    Args:
+        dim (int): dimension of the chains in which the cochain is defined.
+        is_primal (bool): boolean which is True if the cochain is primal
+                          and it is False if it is dual.
+        node_tags (np.array): np.array matrix of node tags.
+        vec (np.array): vectorial representation of the cochain
+    Attributes:
+        dim (int): dimension of the chains in which the cochain is defined.
+        is_primal (bool): boolean which is True if the cochain is primal
+                          and it is False if it is dual.
+        node_tags (np.array): inherited from the class simplicial_complex
+        vec (np.array): vectorial representation of the cochain.
+    """
+    def __init__(self, dim: int, is_primal: bool, node_tags, vec=None):
+        super().__init__(node_tags)
+        self.dim = dim
+        self.is_primal = is_primal
+        self.vec = vec
+
+
+def coboundary_operator(c):
+    """Implements the coboundary operator
+
+    Args:
+        c (Cochain): a cochain
+    Returns:
+        dc (Cochain): the cochain obtained taking the coboundary of c
+    """
+    # initialize dc
+    dc = cochain(dim=c.dim + 1, is_primal=c.is_primal, node_tags=c.node_tags)
+
+    # construct boundary matrix
+    t = c.get_boundary_operators()[c.dim]
+
+    # check if transposition is needed
+    # and compute matrix-vector product
+    if c.is_primal:
+        dc.vec = spmv.spmv_coo(t, c.vec, transpose=True)
+    else:
+        dc.vec = spmv.spmv_coo(t, c.vec, transpose=False)
+    return dc
+
+
+Cochain = cochain
