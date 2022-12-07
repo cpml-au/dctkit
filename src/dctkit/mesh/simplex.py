374889ec6d352ff39f82c4d69ff8b4c97a3e571c
Alessandro Lucantonio <alessandro.lucantonio@gmail.com>
2022-12-07 13:41:42 +0100

    Simplified get_primal_volumes. Fixed some tests.

--- src/dctkit/mesh/simplex.py
+++ src/dctkit/mesh/simplex.py
@@ -8,42 +8,42 @@ class SimplicialComplex:
     """Simplicial complex class.
 
     Args:
-        tet_node_tags (int32 np.array): (num_tet x num_nodes_per_tet) matrix
-        containing the IDs of the nodes belonging to each tetrahedron (or higher
-        level simplex). node_coord (float np.array): Cartesian coordinates
-        (columns) of all the nodes (rows) of the simplicial complex.
+        tet_node_tags (int32 np.array): matrix containing the IDs of the nodes
+        (cols) belonging to each tetrahedron or top-level simplex (rows).
+        node_coord (float np.array): Cartesian coordinates (columns) of all the
+        nodes (rows) of the simplicial complex.
     Attributes:
-        dim (int32): dimension of the complex S_p (list): list in which any
-        entry p is the matrix containing the
+        dim (int32): dimension of the complex
+        S_p (list): list where each entry p is a matrix containing the
                     IDs of the nodes belonging to each p-simplex.
-        circ (list): list in which any entry p is a matrix containing all the
-                     circumcenters of all the p-simplexes.
-        boundary(list): list of the boundary operators. node_coord (float
-        np.array): Cartesian coordinates (columns) of all the nodes (rows) of
-        the simplicial complex.
+        circ (list): list where each entry p is a matrix containing the
+        coordinates of the circumcenters (cols) of all the p-simplexes (rows).
+        boundary (list): list of the boundary matrices at all dimensions (0..dim-1).
+        node_coord (float np.array): Cartesian coordinates (columns) of all the
+        nodes (rows) of the simplicial complex.
+        primal_volumes ():
+        dual_volumes ():
+        B_p ():
     """
 
     def __init__(self, tet_node_tags, node_coord):
-        # Store the coordinates of the nodes
+        # store the coordinates of the nodes
         self.node_coord = node_coord
         self.embedded_dim = node_coord.shape[1]
 
-        # Compute complex dimension from top-level simplices
+        # compute complex dimension from top-level simplices
         self.dim = tet_node_tags.shape[1] - 1
 
-        # S_p is the matrix containing the IDs of the nodes belonging
-        # to each p-simplex
-        self.S_p = [None]*(self.dim + 1)
+        self.S_p = [None] * (self.dim + 1)
         self.S_p[-1] = tet_node_tags
 
-        self.boundary = sl.ShiftedList([None]*self.dim, -1)
-        self.circ = sl.ShiftedList([None]*(self.dim), -1)
-        self.bary_circ = sl.ShiftedList([None]*(self.dim), -1)
-        self.primal_volumes = sl.ShiftedList([None]*(self.dim), -1)
-        self.dual_volumes = sl.ShiftedList([None]*(self.dim), -1)
+        self.boundary = sl.ShiftedList([None] * self.dim, -1)
+        self.circ = sl.ShiftedList([None] * (self.dim), -1)
+        self.bary_circ = sl.ShiftedList([None] * (self.dim), -1)
+        self.primal_volumes = sl.ShiftedList([None] * (self.dim), -1)
+        self.dual_volumes = sl.ShiftedList([None] * (self.dim), -1)
 
-        # TODO: check!
-        self.boundary_simplices = sl.ShiftedList([None]*self.dim, -2)
+        self.B_p = sl.ShiftedList([None] * self.dim, -1)
 
         # populate boundary operators
         self.__get_boundary()
@@ -52,57 +52,49 @@ class SimplicialComplex:
         """Compute all the COO representations of the boundary matrices.
         """
         for p in range(self.dim):
-            # FIXME: initialize with np.empty every boundary
-            if self.dim - p > 1:
-                boundary, vals, b_sim = compute_boundary_COO(self.S_p[self.dim - p])
-                self.boundary_simplices[self.dim - p] = b_sim
-            else:
-                boundary, vals = compute_boundary_COO(self.S_p[self.dim - p])
+            boundary, vals, B_p = compute_boundary_COO(self.S_p[self.dim - p])
 
             self.boundary[self.dim - p] = boundary
+            self.B_p[self.dim - p] = B_p
             self.S_p[self.dim - p - 1] = vals
 
     def get_circumcenters(self):
         """Compute all the circumcenters.
         """
         for p in range(self.dim):
-            S_p = self.S_p[p+1]
+            S_p = self.S_p[p + 1]
             C = np.empty((S_p.shape[0], self.node_coord.shape[1]))
             B = np.empty((S_p.shape[0], S_p.shape[1]))
             for i in range(S_p.shape[0]):
-                C[i, :], B[i, :] = circ.circumcenter(S_p[i, :], self.node_coord)
+                C[i, :], B[i, :] = circ.circumcenter(S_p[i, :],
+                                                     self.node_coord)
             self.circ[p + 1] = C
             self.bary_circ[p + 1] = B
 
     def get_primal_volumes(self):
         """Compute all the primal volumes.
         """
-        unsigned_range = self.dim
-        if self.dim == self.embedded_dim:
-            unsigned_range -= 1
-        for p in range(unsigned_range):
-            S = self.S_p[p+1]
-            rows, _ = S.shape
-            primal_volumes = np.empty(rows)
-            for i in range(rows):
-                primal_volumes[i] = volume.unsigned_volume(S[i, :], self.node_coord)
-            self.primal_volumes[p + 1] = primal_volumes
-        if self.dim == self.embedded_dim:
-            S_p = self.S_p[self.dim]
-            rows, _ = S_p.shape
-            primal_volumes = np.empty(rows)
-            for i in range(rows):
-                primal_volumes[i] = volume.signed_volume(S_p[i, :], self.node_coord)
-            self.primal_volumes[self.dim] = primal_volumes
+        # loop over all p-simplices (1..dim + 1)
+        # (volume of 1-simplices is 1, we do not store it)
+        for p in range(1, self.dim + 1):
+            S = self.S_p[p]
+            num_p_simplices, _ = S.shape
+            primal_volumes = np.empty(num_p_simplices)
+            for i in range(num_p_simplices):
+                if p == self.embedded_dim:
+                    primal_volumes[i] = volume.signed_volume(S[i, :], self.node_coord)
+                else:
+                    primal_volumes[i] = volume.unsigned_volume(S[i, :], self.node_coord)
+            self.primal_volumes[p] = primal_volumes
 
     def get_dual_volumes(self):
         """Compute all the dual volumes.
         """
-        # Loop over simplices at all dimensions
+        # loop over simplices at all dimensions
         # for p in range(self.dim + 1):
         p = 2
-        num_p, num_bnd_simplices = self.boundary_simplices[p].shape
-        num_pm1, _ = self.S_p[p-1].shape
+        num_p, num_bnd_simplices = self.B_p[p].shape
+        num_pm1, _ = self.S_p[p - 1].shape
         dv = np.zeros(num_pm1)
 
         # Loop over p-simplices
@@ -111,14 +103,16 @@ class SimplicialComplex:
 
             for j in range(num_bnd_simplices):
                 # ID of the boundary (p-1)-simplex
-                index = self.boundary_simplices[p][i, j]
+                index = self.B_p[p][i, j]
 
                 # Distance between circumcenters of the p-simplex and the
                 # boundary (p-1)-simplex
-                length = np.linalg.norm(self.circ[p][i, :] - self.circ[p-1][index, :])
+                length = np.linalg.norm(self.circ[p][i, :] -
+                                        self.circ[p - 1][index, :])
 
                 # Find opposite vertex to the (p-1)-simplex
-                opp_vert = list(set(self.S_p[p][i, :])-set(self.S_p[p-1][index, :]))[0]
+                opp_vert = list(
+                    set(self.S_p[p][i, :]) - set(self.S_p[p - 1][index, :]))[0]
                 opp_vert_index = list(self.S_p[p][i, :]).index(opp_vert)
 
                 # Sign of the dual volume of the boundary (p-1)-simplex = sign
@@ -127,9 +121,9 @@ class SimplicialComplex:
                 sign = np.copysign(1, self.bary_circ[p][i, opp_vert_index])
 
                 # Update dual volume of the boundary (p-1)-simplex
-                dv[index] += sign*length
+                dv[index] += sign * length
 
-        self.dual_volumes[p-1] = dv
+        self.dual_volumes[p - 1] = dv
 
 
 def simplex_array_parity(s):
@@ -168,19 +162,25 @@ def simplex_array_parity(s):
 
 
 def compute_boundary_COO(S_p):
-    """Compute the COO representation of the boundary matrix of S_p
+    """Compute the COO representation of the boundary matrix of all p-simplices.
 
     Args:
-        S_p (np.array): matrix of the IDs of the nodes belonging to each
-        p-simplex.
+        S_p (int32 np.array): matrix of the IDs of the nodes (cols) belonging to
+        each p-simplex (rows).
     Returns:
-        boundary_COO (tuple): tuple with the COO representation of the boundary
-        vals (np.array): np.array matrix of node tags per (p-1)-face ordered
-                         lexicographically
+        boundary_COO (tuple): tuple with the COO representation of the boundary.
+        vals (int32 np.array): np.array matrix of node tags per (p-1)-face
+        ordered lexicographically.
     """
+    # number of p-simplices
     num_simplices = S_p.shape[0]
-    faces_per_simplex = S_p.shape[1]
-    num_faces = num_simplices * faces_per_simplex
+    # nodes per p-simplex = p + 1
+    nodes_per_simplex = S_p.shape[1]
+
+    dim = nodes_per_simplex - 1
+
+    # TODO: is there an appropriate name for this?
+    N = num_simplices * nodes_per_simplex
 
     # compute array of relative orientations of the (p-1)-faces wrt the
     # p-simplices
@@ -192,11 +192,11 @@ def compute_boundary_COO(S_p):
     F.sort(axis=1)
 
     # S_(p-1) matrix with repeated (p-1)-simplices and with two extra columns
-    S_pm1_ext = np.empty((num_faces, faces_per_simplex + 1), dtype=np.int32)
+    S_pm1_ext = np.empty((N, nodes_per_simplex + 1), dtype=np.int64)
 
     # find the node IDs of the (p-1)-simplices and store their relative
     # orientations wrt the parent simplex
-    for i in range(faces_per_simplex):
+    for i in range(nodes_per_simplex):
         # remove the i-th column from the S_p matrix and put the result in the
         # appropriate block S_pm1_ext
         rows = S_pm1_ext[num_simplices * i:num_simplices * (i + 1)]
@@ -214,26 +214,33 @@ def compute_boundary_COO(S_p):
     faces_ordered = S_pm1_ext[np.lexsort(S_pm1_ext[:, :-2].T[::-1])]
     values = faces_ordered[:, -2]
     column_index = faces_ordered[:, -1]
-    edge = faces_ordered[:, :-2]
+    faces = faces_ordered[:, :-2]
 
     # compute vals and rows_index
-    vals, rows_index = np.unique(edge, axis=0, return_inverse=True)
+    vals, rows_index = np.unique(faces, axis=0, return_inverse=True)
     boundary_COO = (rows_index, column_index, values)
-    if faces_per_simplex > 2:
+
+    # for triangles and tets, compute B_p explicitly
+    if dim > 1:
         # order faces_ordered w.r.t last column
         faces_ordered_last = faces_ordered[faces_ordered[:, -1].argsort()]
 
         # initialize the matrix of the boundary simplex as an array
-        b_sim = np.empty(edge.shape[0])
+        B_p = np.empty(faces.shape[0], dtype=np.int64)
+
         for i, v in enumerate(vals):
             # find position of vector v in faces_ordered_last[:, :-2]
-            position = np.where((faces_ordered_last[:, :-2] == v).all(axis=1))[0]
+            position = np.where(
+                (faces_ordered_last[:, :-2] == v).all(axis=1))[0]
 
-            # update b_sim in position indices
-            b_sim[position] = i
+            # update B_p in position indices
+            B_p[position] = i
 
-        b_sim = b_sim.reshape(edge.shape[0] // faces_per_simplex, faces_per_simplex)
-        b_sim = b_sim.astype(int)
-        return boundary_COO, vals, b_sim
+        B_p = B_p.reshape(faces.shape[0] // nodes_per_simplex,
+                          nodes_per_simplex)
 
-    return boundary_COO, vals
+    # for edges, B_1 = S_1
+    else:
+        B_p = S_p
+
+    return boundary_COO, vals, B_p
--- tests/test_shifted_list.py
+++ tests/test_shifted_list.py
@@ -4,3 +4,6 @@ from dctkit.math import shifted_list as sl
 def test_shifted_list():
     boundary = sl.ShiftedList([0,1],-1)
     assert boundary[1] == 0
+
+if __name__ == '__main__':
+    test_shifted_list()
