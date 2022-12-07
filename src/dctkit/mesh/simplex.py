cc9f4fefd68b0ff97b5bbbdcef69dd1cc95475cc
Alessandro Lucantonio <alessandro.lucantonio@gmail.com>
2022-12-07 13:49:00 +0100

    Changed notation for S_p and B_p.

--- src/dctkit/mesh/simplex.py
+++ src/dctkit/mesh/simplex.py
@@ -14,7 +14,7 @@ class SimplicialComplex:
         nodes (rows) of the simplicial complex.
     Attributes:
         dim (int32): dimension of the complex
-        S_p (list): list where each entry p is a matrix containing the
+        S (list): list where each entry p is a matrix containing the
                     IDs of the nodes belonging to each p-simplex.
         circ (list): list where each entry p is a matrix containing the
         coordinates of the circumcenters (cols) of all the p-simplexes (rows).
@@ -23,7 +23,7 @@ class SimplicialComplex:
         nodes (rows) of the simplicial complex.
         primal_volumes ():
         dual_volumes ():
-        B_p ():
+        B ():
     """
 
     def __init__(self, tet_node_tags, node_coord):
@@ -34,8 +34,8 @@ class SimplicialComplex:
         # compute complex dimension from top-level simplices
         self.dim = tet_node_tags.shape[1] - 1
 
-        self.S_p = [None] * (self.dim + 1)
-        self.S_p[-1] = tet_node_tags
+        self.S = [None] * (self.dim + 1)
+        self.S[-1] = tet_node_tags
 
         self.boundary = sl.ShiftedList([None] * self.dim, -1)
         self.circ = sl.ShiftedList([None] * (self.dim), -1)
@@ -43,7 +43,7 @@ class SimplicialComplex:
         self.primal_volumes = sl.ShiftedList([None] * (self.dim), -1)
         self.dual_volumes = sl.ShiftedList([None] * (self.dim), -1)
 
-        self.B_p = sl.ShiftedList([None] * self.dim, -1)
+        self.B = sl.ShiftedList([None] * self.dim, -1)
 
         # populate boundary operators
         self.__get_boundary()
@@ -52,24 +52,24 @@ class SimplicialComplex:
         """Compute all the COO representations of the boundary matrices.
         """
         for p in range(self.dim):
-            boundary, vals, B_p = compute_boundary_COO(self.S_p[self.dim - p])
+            boundary, vals, B = compute_boundary_COO(self.S[self.dim - p])
 
             self.boundary[self.dim - p] = boundary
-            self.B_p[self.dim - p] = B_p
-            self.S_p[self.dim - p - 1] = vals
+            self.B[self.dim - p] = B
+            self.S[self.dim - p - 1] = vals
 
     def get_circumcenters(self):
         """Compute all the circumcenters.
         """
-        for p in range(self.dim):
-            S_p = self.S_p[p + 1]
-            C = np.empty((S_p.shape[0], self.node_coord.shape[1]))
-            B = np.empty((S_p.shape[0], S_p.shape[1]))
-            for i in range(S_p.shape[0]):
-                C[i, :], B[i, :] = circ.circumcenter(S_p[i, :],
+        for p in range(1, self.dim + 1):
+            S = self.S[p]
+            C = np.empty((S.shape[0], self.node_coord.shape[1]))
+            B = np.empty((S.shape[0], S.shape[1]))
+            for i in range(S.shape[0]):
+                C[i, :], B[i, :] = circ.circumcenter(S[i, :],
                                                      self.node_coord)
-            self.circ[p + 1] = C
-            self.bary_circ[p + 1] = B
+            self.circ[p] = C
+            self.bary_circ[p] = B
 
     def get_primal_volumes(self):
         """Compute all the primal volumes.
@@ -77,10 +77,11 @@ class SimplicialComplex:
         # loop over all p-simplices (1..dim + 1)
         # (volume of 1-simplices is 1, we do not store it)
         for p in range(1, self.dim + 1):
-            S = self.S_p[p]
+            S = self.S[p]
             num_p_simplices, _ = S.shape
             primal_volumes = np.empty(num_p_simplices)
             for i in range(num_p_simplices):
+                # TODO: vectorization of signed_volume and unsigned_volume
                 if p == self.embedded_dim:
                     primal_volumes[i] = volume.signed_volume(S[i, :], self.node_coord)
                 else:
@@ -93,8 +94,8 @@ class SimplicialComplex:
         # loop over simplices at all dimensions
         # for p in range(self.dim + 1):
         p = 2
-        num_p, num_bnd_simplices = self.B_p[p].shape
-        num_pm1, _ = self.S_p[p - 1].shape
+        num_p, num_bnd_simplices = self.B[p].shape
+        num_pm1, _ = self.S[p - 1].shape
         dv = np.zeros(num_pm1)
 
         # Loop over p-simplices
@@ -103,7 +104,7 @@ class SimplicialComplex:
 
             for j in range(num_bnd_simplices):
                 # ID of the boundary (p-1)-simplex
-                index = self.B_p[p][i, j]
+                index = self.B[p][i, j]
 
                 # Distance between circumcenters of the p-simplex and the
                 # boundary (p-1)-simplex
@@ -112,8 +113,8 @@ class SimplicialComplex:
 
                 # Find opposite vertex to the (p-1)-simplex
                 opp_vert = list(
-                    set(self.S_p[p][i, :]) - set(self.S_p[p - 1][index, :]))[0]
-                opp_vert_index = list(self.S_p[p][i, :]).index(opp_vert)
+                    set(self.S[p][i, :]) - set(self.S[p - 1][index, :]))[0]
+                opp_vert_index = list(self.S[p][i, :]).index(opp_vert)
 
                 # Sign of the dual volume of the boundary (p-1)-simplex = sign
                 # of the barycentric coordinate of the circumcenter of the
@@ -161,11 +162,11 @@ def simplex_array_parity(s):
     return trans
 
 
-def compute_boundary_COO(S_p):
+def compute_boundary_COO(S):
     """Compute the COO representation of the boundary matrix of all p-simplices.
 
     Args:
-        S_p (int32 np.array): matrix of the IDs of the nodes (cols) belonging to
+        S (int32 np.array): matrix of the IDs of the nodes (cols) belonging to
         each p-simplex (rows).
     Returns:
         boundary_COO (tuple): tuple with the COO representation of the boundary.
@@ -173,9 +174,9 @@ def compute_boundary_COO(S_p):
         ordered lexicographically.
     """
     # number of p-simplices
-    num_simplices = S_p.shape[0]
+    num_simplices = S.shape[0]
     # nodes per p-simplex = p + 1
-    nodes_per_simplex = S_p.shape[1]
+    nodes_per_simplex = S.shape[1]
 
     dim = nodes_per_simplex - 1
 
@@ -184,11 +185,11 @@ def compute_boundary_COO(S_p):
 
     # compute array of relative orientations of the (p-1)-faces wrt the
     # p-simplices
-    orientations = 1 - 2 * simplex_array_parity(S_p)
+    orientations = 1 - 2 * simplex_array_parity(S)
 
-    # sort the rows of S_p lexicographically
+    # sort the rows of S lexicographically
     # FIXME: avoid making a copy and sorting every time
-    F = S_p.copy()
+    F = S.copy()
     F.sort(axis=1)
 
     # S_(p-1) matrix with repeated (p-1)-simplices and with two extra columns
@@ -197,7 +198,7 @@ def compute_boundary_COO(S_p):
     # find the node IDs of the (p-1)-simplices and store their relative
     # orientations wrt the parent simplex
     for i in range(nodes_per_simplex):
-        # remove the i-th column from the S_p matrix and put the result in the
+        # remove the i-th column from the S matrix and put the result in the
         # appropriate block S_pm1_ext
         rows = S_pm1_ext[num_simplices * i:num_simplices * (i + 1)]
         rows[:, :i] = F[:, :i]
@@ -220,27 +221,26 @@ def compute_boundary_COO(S_p):
     vals, rows_index = np.unique(faces, axis=0, return_inverse=True)
     boundary_COO = (rows_index, column_index, values)
 
-    # for triangles and tets, compute B_p explicitly
+    # for triangles and tets, compute B explicitly
     if dim > 1:
         # order faces_ordered w.r.t last column
         faces_ordered_last = faces_ordered[faces_ordered[:, -1].argsort()]
 
         # initialize the matrix of the boundary simplex as an array
-        B_p = np.empty(faces.shape[0], dtype=np.int64)
+        B = np.empty(faces.shape[0], dtype=np.int64)
 
         for i, v in enumerate(vals):
             # find position of vector v in faces_ordered_last[:, :-2]
             position = np.where(
                 (faces_ordered_last[:, :-2] == v).all(axis=1))[0]
 
-            # update B_p in position indices
-            B_p[position] = i
+            # update B in position indices
+            B[position] = i
 
-        B_p = B_p.reshape(faces.shape[0] // nodes_per_simplex,
-                          nodes_per_simplex)
+        B = B.reshape(faces.shape[0] // nodes_per_simplex, nodes_per_simplex)
 
     # for edges, B_1 = S_1
     else:
-        B_p = S_p
+        B = S
 
-    return boundary_COO, vals, B_p
+    return boundary_COO, vals, B
