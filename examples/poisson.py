cf8a924f8ad360e5701076f8715dbffd5965bd92
Simone Manti <manti998@gmail.com>
2022-12-12 16:04:23 +0100

    Fixed poisson.py and test_poisson.py. Documentation needed

--- examples/poisson.py
+++ examples/poisson.py
@@ -17,17 +17,15 @@ def Poisson(c, k, boundary_values):
     """
     # extract boundary values and their positions
     pos, value = boundary_values
-
     # create a new cochain that takes in account boundary values
     c_new = C.Cochain(c.dim, c.is_primal, c.complex,
-                      np.empty(len(pos) + len(c.coeffs)))
+                      np.empty(len(c.coeffs) + len(pos)))
     # create a mask to track indexes other than pos
     mask = np.ones(len(pos) + len(c.coeffs), bool)
     mask[pos] = False
     c_new.coeffs[pos] = value
     c_new.coeffs[mask] = c.coeffs
     c = c_new
-
     # compute the coboundary
     c_1 = C.coboundary(c)
     # compute star
@@ -38,3 +36,30 @@ def Poisson(c, k, boundary_values):
     # coboundary again to obtain a dual 2-cochain
     p = C.coboundary(h)
     return p
+
+
+def Poisson_vec_operator(x, S, k, boundary_values):
+    # S is a predefined SimplicialComplex object
+    c = C.Cochain(0, True, S, x)
+    p = Poisson(c, k, boundary_values)
+    w = p.coeffs
+    return w
+
+
+def obj_poisson(x, f, S, k, boundary_values, gamma):
+    pos, value = boundary_values
+    Ax = Poisson_vec_operator(x, S, k, boundary_values)
+    r = Ax - f
+    penalty = np.sum((x[pos] - value)**2)
+    energy = 0.5*np.linalg.norm(r)**2 + 0.5*gamma*penalty
+    return energy
+
+
+def grad_poisson(x, f, S, k, boundary_values, gamma):
+    pos, value = boundary_values
+    Ax = Poisson_vec_operator(x, S, k, boundary_values)
+    grad_r = Poisson_vec_operator(Ax - f, S, k, boundary_values)
+    grad_penalty = np.zeros(len(Ax))
+    grad_penalty[pos] = x[pos] - value
+    grad_energy = grad_r + gamma*grad_penalty
+    return grad_energy
--- tests/test_poisson.py
+++ tests/test_poisson.py
@@ -1,49 +1,41 @@
 import numpy as np
-from scipy.sparse.linalg import cg, LinearOperator
+from scipy.optimize import minimize
 from dctkit.mesh import simplex, util
-from dctkit.dec import cochain as C
-from examples import poisson
+from examples import poisson as p
 import os
 
 cwd = os.path.dirname(simplex.__file__)
-filename = "test1.msh"
-full_path = os.path.join(cwd, filename)
-numNodes, numElements, S_2, node_coord = util.read_mesh(full_path)
-
-print(f"The number of nodes in the mesh is {numNodes}")
-print(f"The number of faces in the mesh is {numElements}")
-print(f"The vectorization of the face matrix is \n {S_2}")
-
-# initialize simplicial complex
-S = simplex.SimplicialComplex(S_2, node_coord)
-S.get_circumcenters()
-S.get_primal_volumes()
-S.get_dual_volumes()
-S.get_hodge_star()
-# TODO: initialize diffusivity
-k = 0.1
-# TODO: initialize boundary_values
-boundary_values = (np.array([0, 1, 2, 3]), np.ones(4))
-
-
-def Poisson_vec_operator(v):
-    # S is a predefined SimplicialComplex object
-    c = C.Cochain(0, True, S, v)
-    # k and boundary_values are predefined
-    p = poisson.Poisson(c, k, boundary_values)
-    w = p.coeffs
-    print(w)
-    return w
 
 
 def test_poisson():
+    filename = "test1.msh"
+    full_path = os.path.join(cwd, filename)
+    numNodes, numElements, S_2, node_coord = util.read_mesh(full_path)
+
+    print(f"The number of nodes in the mesh is {numNodes}")
+    print(f"The number of faces in the mesh is {numElements}")
+    print(f"The vectorization of the face matrix is \n {S_2}")
+
+    # initialize simplicial complex
+    S = simplex.SimplicialComplex(S_2, node_coord)
+    S.get_circumcenters()
+    S.get_primal_volumes()
+    S.get_dual_volumes()
+    S.get_hodge_star()
+    # TODO: initialize diffusivity
+    k = 1
+    # TODO: initialize boundary_values
+    boundary_values = (np.array([0, 1, 2, 3]), np.arange(4))
+    pos, values = boundary_values
     # TODO: initialize external sources
-    zero_coch_dim = node_coord.shape[0]
-    f_coeffs = np.zeros(zero_coch_dim)
-    Poisson = LinearOperator((zero_coch_dim, zero_coch_dim),
-                             matvec=Poisson_vec_operator)
-    u_coeffs = cg(Poisson, f_coeffs)
-    print(u_coeffs)
+    dim_0 = node_coord.shape[0]
+    f = np.zeros(dim_0)
+    u_0 = np.ones(5)
+    u_0[-1] = 5
+    args = (f, S, k, boundary_values, 1)
+    u = minimize(fun=p.obj_poisson, x0=u_0, args=args, method='CG', jac=p.grad_poisson,
+                 options={"disp": True})
+    print(u)
 
 
 if __name__ == '__main__':
