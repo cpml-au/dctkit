[WIP] Fixed double precision issue in SPMV. Poisson test converging now.

src/dctkit/apps/poisson.py
@@ -74,6 +74,7 @@ def obj_poisson(x, f, S, k, boundary_values, gamma):
     # \sum_i (x_i - value_i)^2
     penalty = np.sum((x[pos] - value)**2)
     energy = 0.5*np.linalg.norm(r)**2 + 0.5*gamma*penalty
+    print(energy)
     return energy
 
 
@@ -99,7 +100,5 @@ def grad_poisson(x, f, S, k, boundary_values, gamma):
     grad_r = poisson_vec_operator(Ax - f, S, k)
     grad_penalty = np.zeros(len(Ax))
     grad_penalty[pos] = x[pos] - value
-    print(grad_penalty)
     grad_energy = grad_r + gamma*grad_penalty
-    print(np.linalg.norm(grad_energy))
     return grad_energy

tests/test_poisson.py
@@ -38,15 +38,17 @@ def test_poisson():
     S.get_hodge_star()
 
     # TODO: initialize diffusivity
-    k = 1
+    k = 1.
     # TODO: initialize boundary_values
     boundary_values = (np.array(bnodes), np.zeros(len(bnodes)))
     # TODO: initialize external sources
     dim_0 = S.num_nodes
-    f = np.ones(dim_0)
-    u_0 = np.random.rand(dim_0)
+    f = 1*np.ones(dim_0)
+    u_0 = 0.01*np.random.rand(dim_0)
+    # u_0 = np.zeros(dim_0)
+    ic(u_0)
 
-    gamma = 10000
+    gamma = 10.
     args = (f, S, k, boundary_values, gamma)
     u = minimize(fun=p.obj_poisson, x0=u_0, args=args, method='CG',
                  jac=p.grad_poisson, options={'disp': 1})

tests/test_spmv.py
@@ -5,19 +5,20 @@ import numpy as np
 def test_spmv_coo():
     rows = np.array([0, 0, 1, 2], dtype=np.int32)
     cols = np.array([0, 1, 1, 2], dtype=np.int32)
-    vals = np.array([1, 2, 3, 5], dtype=np.int32)
+    vals = np.array([1., 2., 3., 5.])
     A = [rows, cols, vals]
 
-    v = np.array([0, 1, 2], dtype=np.int32)
-    result_true = np.array([2, 3, 10], dtype=np.int32)
+    v = np.array([0., 1., 2.])
+    result_true = np.array([2., 3., 10.])
     result = spmv.spmv_coo(A, v)
     print(result)
-    assert np.array_equal(result, result_true)
+    assert np.allclose(result, result_true)
 
-    result_transpose_true = np.array([0, 3, 10])
+    result_transpose_true = np.array([0., 3., 10.])
     result = spmv.spmv_coo(A, v, transpose=True)
     print(result)
-    assert np.array_equal(result, result_transpose_true)
+    print(result.dtype)
+    assert np.allclose(result, result_transpose_true)
 
 
 if __name__ == '__main__':