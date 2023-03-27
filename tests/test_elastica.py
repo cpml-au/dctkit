017b93cc96941f0c2e39a42026e7d8583f3e8f09
Simone Manti <manti998@gmail.com>
2023-03-27 11:32:34 +0200

    [WIP] Add the first dataset for test_elastica and implemented new bilevel. They still does not work.

--- tests/test_elastica.py
+++ tests/test_elastica.py
@@ -75,7 +75,7 @@ def test_elastica(is_bilevel=False):
         print(fval)
         # assert fval < 1e-3
 
-    if not is_bilevel:
+    else:
         B = 1.
         theta_0 = 0.1*np.random.rand(num_nodes).astype(dt.float_dtype)
 
@@ -134,14 +134,26 @@ def test_elastica(is_bilevel=False):
     plt.show()
 
 
-def test_elastica_data():
+def test_elastica_data(data_type, is_bilevel=False):
+    np.random.seed(42)
+    # load true data
+    filename = os.path.join(os.path.dirname(__file__), "Data1_elastica.csv")
+    data = np.genfromtxt(filename, delimiter=',')
+    # submatrix of all x and y
+    data_xy = data[2:, 1:]
+    # submatrix of F
+    data_F = data[0, :]
+    data_F = data_F[1::2]
+    x_true, y_true = data_xy[:, 2*data_type], data_xy[:, 2*data_type + 1]
+
     density = 1
     num_fem_elements = 10
+    h = 1/num_fem_elements
     R_0 = 1.2
     R_1 = 0.3
-    L = 1.52
+    L = 1.524
     rho = R_1/R_0
-    F = 8.896
+    F = data_F[data_type]
     S = get_elastica_mesh(density, num_fem_elements)
     node_coord = S.node_coord[:, 0]
     num_nodes = S.num_nodes
@@ -149,50 +161,113 @@ def test_elastica_data():
     # define I_0
     I_0 = np.pi/4 * R_0**4
     # define I
-    I = I_0*(1 + 1/L*(1 - rho)*node_coord)**4
-    I_coch = C.CochainP0(complex=S, coeffs=I)
+    # I = I_0*(1 + 1/L*(1 - rho)*node_coord)**4
+    I = I_0*(1 + 1/L*(1 - rho) * 1/num_fem_elements)**4 * \
+        np.ones(num_fem_elements*density, dtype=dt.float_dtype)
+    # I_coch = C.CochainP0(complex=S, coeffs=I)
+    I_coch = C.CochainD0(complex=S, coeffs=I)
 
-    def energy_elastica(theta: np.array, E: float) -> float:
-        # define A
-        A = F*L**2/(E*I_0)
-        # define B
-        B = C.CochainD0(complex=S, coeffs=E*C.star(C.coboundary(I_coch)).coeffs)
-        theta = C.CochainD1(complex=S, coeffs=theta)
-        const = C.CochainD1(complex=S, coeffs=A *
-                            np.ones(num_nodes, dtype=dt.float_dtype))
-        curvature = C.codifferential(theta)
-        momentum = C.cochain_mul(B, curvature)
-        energy = 0.5*C.inner_product(momentum, curvature) - \
-            C.inner_product(const, C.sin(theta))
-        return energy
+    if is_bilevel:
+        # get theta_true
+        theta_true = np.empty(num_nodes)
+        theta_true[0] = 0
+        for i in range(num_fem_elements-1):
+            theta_true[i + 1] = np.arccos((x_true[i + 1] - x_true[i])/h)
 
-    jac = jit(grad(energy_elastica))
+        theta_true[-1] = theta_true[-2]
 
-    # define linear constraint theta(0) = 0, delta_theta[-1] = 0
-    cons = ({'type': 'eq', 'fun': lambda x: x[0]},
-            {'type': 'eq', 'fun': lambda x: x[num_nodes-2] - x[num_nodes-1]})
+        print(theta_true)
+        theta_0 = 0.1*np.random.rand(num_nodes-2).astype(dt.float_dtype)
 
-    theta_0 = 0.1*np.random.rand(num_nodes).astype(dt.float_dtype)
-    E = 6.
-    res = minimize(fun=energy_elastica, x0=theta_0, args=(E), method="SLSQP",
-                   jac=jac, constraints=cons, options={'disp': 1, 'maxiter': 500})
-    print(res)
-    theta = res.x
+        def energy_elastica_constr(theta: np.array, E: float) -> float:
+            theta = jnp.insert(theta, 0, 0)
+            theta = jnp.append(theta, theta[-1])
+            # define A
+            A = F*L**2/(E*I_0)
+            # define B
+            # B = C.CochainD0(complex=S, coeffs=E*C.star(C.coboundary(I_coch)).coeffs)
+            B = C.CochainD0(complex=S, coeffs=E*I_coch.coeffs)
+            theta = C.CochainD1(complex=S, coeffs=theta)
+            const = C.CochainD1(complex=S, coeffs=A *
+                                np.ones(num_nodes, dtype=dt.float_dtype))
+            curvature = C.codifferential(theta)
+            momentum = C.cochain_mul(B, curvature)
+            energy = 0.5*C.inner_product(momentum, curvature) - \
+                C.inner_product(const, C.sin(theta))
+            return energy
+
+        '''
+        def obj_fun(theta_guess: np.array, B_guess: float) -> float:
+            theta_guess = jnp.insert(theta_guess, 0, 0)
+            theta_guess = jnp.append(theta_guess, theta_guess[-1])
+            # recover x and y
+            x = np.empty(num_fem_elements + 1)
+            y = np.empty(num_fem_elements + 1)
+            x[0] = 0
+            y[0] = 0
+            for i in range(num_fem_elements):
+                x[i + 1] = x[i] + h * np.cos(theta_guess[i])
+                y[i + 1] = y[i] + h * np.sin(theta_guess[i])
+            return jnp.sum(jnp.square(x-x_true)) + jnp.sum(jnp.square(y-y_true))
+        '''
+
+        def obj_fun(theta_guess: np.array, B_guess: float) -> float:
+            theta_guess = jnp.insert(theta_guess, 0, 0)
+            theta_guess = jnp.append(theta_guess, theta_guess[-1])
+            return jnp.sum(jnp.square(theta_guess-theta_true))
+
+        prb = optctrl.OptimalControlProblem(
+            objfun=obj_fun, state_en=energy_elastica_constr, state_dim=num_nodes-2)
+        E_0 = 6*np.ones(1, dtype=dt.float_dtype)
+        theta, E, fval = prb.run(theta_0, E_0, tol=1e-2)
+        # extend theta
+        theta = np.insert(theta, 0, 0)
+        theta = np.append(theta, theta[-1])
+        print(f"The optimal E is {E[0]}")
+        print(fval)
+        # assert fval < 1e-3
+
+    else:
+        E_all = np.array([6., 5.6, 6.5, 5.8])
+        E = E_all[data_type]
+        theta_0 = 1*np.random.rand(num_nodes).astype(dt.float_dtype)
+
+        def energy_elastica(theta: np.array, E: float) -> float:
+            # define A
+            A = F*L**2/(E*I_0)
+            # define B
+            # B = C.CochainD0(complex=S, coeffs=E*C.star(C.coboundary(I_coch)).coeffs)
+            B = C.CochainD0(complex=S, coeffs=E*I_coch.coeffs)
+            # print(B.coeffs)
+            theta = C.CochainD1(complex=S, coeffs=theta)
+            const = C.CochainD1(complex=S, coeffs=A *
+                                np.ones(num_nodes, dtype=dt.float_dtype))
+            curvature = C.codifferential(theta)
+            momentum = C.cochain_mul(B, curvature)
+            energy = 0.5*C.inner_product(momentum, curvature) - \
+                C.inner_product(const, C.sin(theta))
+            return energy
+
+        jac = jit(grad(energy_elastica))
+
+        # define linear constraint theta(0) = 0, delta_theta[-1] = 0
+        cons = ({'type': 'eq', 'fun': lambda x: x[0]},
+                {'type': 'eq', 'fun': lambda x: x[num_nodes-2] - x[num_nodes-1]})
+        res = minimize(fun=energy_elastica, x0=theta_0, args=(E), method="SLSQP",
+                       jac=jac, constraints=cons, options={'disp': 1, 'maxiter': 500})
+        print(res)
+        theta = res.x
+        theta = theta[::density]
 
     # recover x and y
     x = np.empty(num_fem_elements + 1)
     y = np.empty(num_fem_elements + 1)
     x[0] = 0
     y[0] = 0
-    h = 1/num_fem_elements
     for i in range(num_fem_elements):
         x[i + 1] = x[i] + h * np.cos(theta[i])
         y[i + 1] = y[i] + h * np.sin(theta[i])
 
-    x_true = np.array([0, 0.006, 0.019, 0.058, 0.104, 0.169,
-                       0.247, 0.331, 0.429, 0.519, 0.604])
-    y_true = np.array([0, 0.084, 0.162, 0.253, 0.325, 0.383,
-                       0.442, 0.481, 0.494, 0.500, 0.506])
     print(x)
     print(y)
     print(np.linalg.norm(x - x_true))
@@ -206,4 +281,4 @@ def test_elastica_data():
 
 if __name__ == "__main__":
     # test_elastica(is_bilevel=True)
-    test_elastica_data()
+    test_elastica_data(data_type=0, is_bilevel=False)
