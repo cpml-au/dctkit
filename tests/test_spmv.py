e318f4bc6a432da65d919455a6a210ee96a9c86e
Alessandro Lucantonio <alessandro.lucantonio@gmail.com>
2023-05-09 08:46:12 +0200

    [WIP] Fixed shape parameter when calling spmv. Elastica and optctrl test segfault.

--- tests/test_spmv.py
+++ tests/test_spmv.py
@@ -5,18 +5,20 @@ import numpy as np
 
 def test_spmv_coo(setup_test):
     int_dtype = dctkit.int_dtype
+    float_dtype = dctkit.float_dtype
     rows = np.array([0, 0, 1, 2], dtype=int_dtype)
     cols = np.array([0, 1, 1, 2], dtype=int_dtype)
-    vals = np.array([1, 2, 3, 5], dtype=int_dtype)
+    vals = np.array([1, 2, 3, 5], dtype=float_dtype)
     A = [rows, cols, vals]
 
-    v = np.array([0, 1, 2], dtype=int_dtype)
-    result_true = np.array([2, 3, 10], dtype=int_dtype)
+    # multiplication by int-valued arrays not supported
+    v = np.array([0, 1, 2], dtype=float_dtype)
+    result_true = np.array([2, 3, 10], dtype=float_dtype)
     result = spmv.spmv_coo_jax(A, v, shape=(3, 3))
     print(result)
     assert np.allclose(result, result_true)
 
-    result_transpose_true = np.array([0, 3, 10], dtype=int_dtype)
-    result = spmv.spmv_coo_jax(A, v, transpose=True, shape=(3,3))
+    result_transpose_true = np.array([0, 3, 10], dtype=float_dtype)
+    result = spmv.spmv_coo_jax(A, v, transpose=True, shape=(3, 3))
     print(result)
     assert np.allclose(result, result_transpose_true)
