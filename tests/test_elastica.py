aba2eae185e72ef49a43480c3099471175f82a93
Alessandro Lucantonio <alessandro.lucantonio@gmail.com>
2023-05-23 11:26:01 +0200

    Code cleanup in Elastica test.

--- tests/test_elastica.py
+++ tests/test_elastica.py
@@ -8,6 +8,42 @@ import os
 from dctkit.apps import elastica as el
 import numpy.typing as npt
 from dctkit.dec import cochain as C
+from typing import Tuple
+
+
+def compute_transform_theta_xy(num_nodes: int) -> npt.NDArray:
+    # bidiagonal matrix to transform theta in (x,y)
+    diag = [1]*(num_nodes)
+    upper_diag = [-1]*(num_nodes-1)
+    upper_diag[0] = 0
+    diags = [diag, upper_diag]
+    transform = sparse.diags(diags, [0, -1]).toarray()
+    transform[1, 0] = -1
+    return transform
+
+
+def reconstruct_xy(theta: Array, h: float, num_nodes: int) -> Tuple[Array, Array]:
+    transform = compute_transform_theta_xy(num_nodes)
+    cos_theta = h*jnp.cos(theta)
+    sin_theta = h*jnp.sin(theta)
+    b_x = jnp.insert(cos_theta, 0, 0)
+    b_y = jnp.insert(sin_theta, 0, 0)
+    x = jnp.linalg.solve(transform, b_x)
+    y = jnp.linalg.solve(transform, b_y)
+    return x, y
+
+
+def compute_true_solution(data: npt.NDArray, sampling: int,
+                          num_elements: int) -> Tuple[npt.NDArray, npt.NDArray,
+                                                      npt.NDArray]:
+    x_true = data[:, 1][::sampling]
+    y_true = data[:, 2][::sampling]
+    theta_true = np.empty(num_elements, dtype=dt.float_dtype)
+    for i in range(num_elements):
+        theta_true[i] = np.arctan(
+            (y_true[i+1]-y_true[i])/(x_true[i+1]-x_true[i]))
+
+    return theta_true, x_true, y_true
 
 
 def test_elastica_energy(setup_test):
@@ -30,24 +66,11 @@ def test_elastica_energy(setup_test):
     ela = el.ElasticaProblem(num_elements=num_elements, L=L, rho=1.)
     num_nodes = ela.S.num_nodes
 
-    # bidiagonal matrix to transform theta in (x,y)
-    diag = [1]*(num_nodes)
-    upper_diag = [-1]*(num_nodes-1)
-    upper_diag[0] = 0
-    diags = [diag, upper_diag]
-    transform = sparse.diags(diags, [0, -1]).toarray()
-    transform[1, 0] = -1
-
     # initial guess for the angles (EXCEPT THE FIRST ANGLE, FIXED BY BC)
     theta_0 = 0.1*np.random.rand(num_elements-1).astype(dt.float_dtype)
 
     # compute true solution
-    x_true = data[:, 1][::sampling]
-    y_true = data[:, 2][::sampling]
-    theta_true = np.empty(num_elements, dtype=dt.float_dtype)
-    for i in range(num_elements):
-        theta_true[i] = np.arctan(
-            (y_true[i+1]-y_true[i])/(x_true[i+1]-x_true[i]))
+    theta_true, x_true, y_true = compute_true_solution(data, sampling, num_elements)
 
     # state function: stationarity conditions of the elastic energy
     def statefun(x: npt.NDArray, theta_0: npt.NDArray, F: float) -> Array:
@@ -78,12 +101,7 @@ def test_elastica_energy(setup_test):
     theta = np.insert(theta, 0, theta_true[0])
 
     # reconstruct x, y
-    cos_theta = h*jnp.cos(theta)
-    sin_theta = h*jnp.sin(theta)
-    b_x = jnp.insert(cos_theta, 0, 0)
-    b_y = jnp.insert(sin_theta, 0, 0)
-    x = jnp.linalg.solve(transform, b_x)
-    y = jnp.linalg.solve(transform, b_y)
+    x, y = reconstruct_xy(theta, h, num_nodes)
 
     error = np.linalg.norm(x - x_true) + np.linalg.norm(y - y_true)
     assert error <= 2e-2
@@ -113,21 +131,8 @@ def test_elastica_equation(setup_test):
     ela = el.ElasticaProblem(num_elements=num_elements, L=L, rho=1.)
     num_nodes = ela.S.num_nodes
 
-    # bidiagonal matrix to transform theta in (x,y)
-    diag = [1]*(num_nodes)
-    upper_diag = [-1]*(num_nodes-1)
-    upper_diag[0] = 0
-    diags = [diag, upper_diag]
-    transform = sparse.diags(diags, [0, -1]).toarray()
-    transform[1, 0] = -1
-
     # compute true solution
-    x_true = data[:, 1][::sampling]
-    y_true = data[:, 2][::sampling]
-    theta_true = np.empty(num_elements, dtype=dt.float_dtype)
-    for i in range(num_elements):
-        theta_true[i] = np.arctan(
-            (y_true[i+1]-y_true[i])/(x_true[i+1]-x_true[i]))
+    theta_true, x_true, y_true = compute_true_solution(data, sampling, num_elements)
 
     # initial guess for the angles (EXCEPT PRESCRIBED ANGLE AT LEFT END)
     theta_0 = np.zeros(num_elements-1, dtype=dt.float_dtype)
@@ -138,12 +143,13 @@ def test_elastica_equation(setup_test):
     intc[-1] = 0.
     int_coch = C.CochainP0(ela.S, intc)
 
+    # cochain to zero residual on elements where BC is prescribed
     mask = np.ones(num_elements, dtype=dt.float_dtype)
     mask[0] = 0.
     mask_coch = C.CochainP1(ela.S, mask)
 
     def obj(x: npt.NDArray) -> Array:
-        # add boundary conditions
+        # apply Dirichlet BC at left end
         theta = jnp.insert(x, 0, theta_true[0])
         theta_coch = C.CochainD0(ela.S, theta)
 
@@ -169,12 +175,7 @@ def test_elastica_equation(setup_test):
     theta = jnp.insert(sol, 0, theta_true[0])
 
     # reconstruct x, y
-    cos_theta = h*jnp.cos(theta)
-    sin_theta = h*jnp.sin(theta)
-    b_x = jnp.insert(cos_theta, 0, 0)
-    b_y = jnp.insert(sin_theta, 0, 0)
-    x = jnp.linalg.solve(transform, b_x)
-    y = jnp.linalg.solve(transform, b_y)
+    x, y = reconstruct_xy(theta, h, num_nodes)
 
     error = np.linalg.norm(x - x_true) + np.linalg.norm(y - y_true)
     assert error <= 2e-2
