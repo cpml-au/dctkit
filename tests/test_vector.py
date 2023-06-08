74e168755ea3107d273804dae35134413c4b91d8
Simone Manti <manti998@gmail.com>
2023-06-08 10:02:43 +0200

    Fixed various issues in vector.py and simplex.py. Added documentation in linear_elasticity.py

--- tests/test_vector.py
+++ tests/test_vector.py
@@ -20,12 +20,12 @@ def test_vector_cochain(setup_test):
     T_coeffs = np.ones((S.embedded_dim, S.embedded_dim,
                        S.S[2].shape[0]), dtype=dt.float_dtype)
     v = V.DiscreteVectorFieldD(S, v_coeffs)
-    T = V.DiscreteTensorFieldD(S, T_coeffs)
+    T = V.DiscreteTensorFieldD(S, T_coeffs, 2)
     c_v = V.flat_DPD(v)
     c_T = V.flat_DPD(T)
     c_v_true_coeffs = S.dual_edges_vectors.sum(axis=1)
     c_T_true_coeffs = np.ones((12, 3), dtype=dt.float_dtype)
     c_T_true_coeffs = c_v_true_coeffs[:, None]*c_T_true_coeffs
 
-    assert jnp.allclose(c_v.coeffs, c_v_true_coeffs)
-    assert jnp.allclose(c_T.coeffs, c_T_true_coeffs)
+    assert np.allclose(c_v.coeffs, c_v_true_coeffs)
+    assert np.allclose(c_T.coeffs, c_T_true_coeffs)
