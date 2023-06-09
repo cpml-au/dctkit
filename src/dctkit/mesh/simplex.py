[WIP] Fixed metric computation bug. Another bug still occurs.

src/dctkit/mesh/simplex.py
@@ -51,6 +51,7 @@ class SimplicialComplex:
         self.int_dtype = dctkit.int_dtype
         self.is_well_centered = is_well_centered
         self.bnd_faces_tags = bnd_faces_tags
+        self.change_basis_matrix = None
 
         # compute complex dimension from top-level simplices
         self.dim = tet_node_tags.shape[1] - 1
@@ -291,16 +292,30 @@ class SimplicialComplex:
         # corresponding 2-simplex
         primal_edges_per_2_simplex = primal_edge_vectors[B]
         # extract the first two rows, i.e. basis vectors, for each 3x3 matrix
+        # NOTE: basis_vectors is arranged row-wise!
         basis_vectors = primal_edges_per_2_simplex[:, :-1, :]
-        # extract metric multiarray in the local reference frame
-        metric_local = basis_vectors @ jnp.transpose(
-            basis_vectors, axes=(0, 2, 1))
-        # change basis to the global reference frame
-        print(basis_vectors[:, :, :-1])
-        global_to_local = jnp.transpose(basis_vectors[:, :, :-1], axes=(0, 2, 1))
-        global_to_local_inv = jnp.linalg.inv(global_to_local)
-        metric_global = (global_to_local_inv @ metric_local) @ global_to_local
-        return metric_global
+        # to compute the reference metric, we need to compute the change
+        # of basis matrix from the local to the global frame.
+        # This matrix is the inverse of the matrix having as columns the basis
+        # vectors (2x1 vectors) and since basis_vectors is the multiarray of the
+        # embeddeded coordinate in R^3 we have to discard the last column
+        basis_vectors_reshaped = jnp.transpose(
+            basis_vectors[:, :, :-1], axes=(0, 2, 1))
+        if self.change_basis_matrix is None:
+            basis_vectors_reshaped_inv = jnp.linalg.inv(basis_vectors_reshaped)
+            self.change_basis_matrix = basis_vectors_reshaped_inv
+        # we have to compute for any 2x2 matrix A_i in the multiarray change_basis_matrix
+        # the product A_i x_i, where x_i is the  i-th matrix having the coordinates of
+        # the basis_vectors of the i-th simplex arranged column-wise.
+        basis_vectors_global_frameT = self.change_basis_matrix @ basis_vectors_reshaped
+        # to compute the current metric we need to arrange basis-vectors in the global
+        # frame row-wise
+        basis_vectors_global_frame = np.transpose(
+            basis_vectors_global_frameT, axes=(0, 2, 1))
+        # extract metric multiarray in the global frame
+        metric = basis_vectors_global_frame @ jnp.transpose(
+            basis_vectors_global_frame, axes=(0, 2, 1))
+        return metric
 
 
 def __simplex_array_parity(s):

tests/test_linear_elasticity.py
@@ -10,10 +10,12 @@ from jax import grad
 
 def test_linear_elasticity(setup_test):
     lc = 0.5
-    # _, _, S_2, node_coords, bnd_faces_tags = util.generate_square_mesh(lc)
-    _, _, S_2, node_coords, bnd_faces_tags = util.generate_hexagon_mesh(1, 1)
+    _, _, S_2, node_coords, bnd_faces_tags = util.generate_square_mesh(lc)
+    # _, _, S_2, node_coords, bnd_faces_tags = util.generate_hexagon_mesh(1, 1)
     bnodes, _ = gmsh.model.mesh.getNodesForPhysicalGroup(1, 1)
     bnodes -= 1
+    print(bnodes)
+    print(S_2)
     bnodes = bnodes.astype(dt.int_dtype)
     S = simplex.SimplicialComplex(
         S_2, node_coords, bnd_faces_tags=bnd_faces_tags, is_well_centered=True)
@@ -26,17 +28,21 @@ def test_linear_elasticity(setup_test):
 
     mu_ = 1.
     lambda_ = 10.
-    gamma = 10000.
+    gamma = 100000.
+    # FIXME: temporary setting
+    idx = [1, 3, 5]
+    value = S.node_coord[idx, :]
     num_faces = S.S[2].shape[0]
-    print(num_faces)
     embedded_dim = S.embedded_dim
     ela = LinearElasticity(S=S, mu_=mu_, lambda_=lambda_)
     f = np.zeros((num_faces, (embedded_dim-1)))
-    f[:, 1] = 0.1*np.ones(num_faces)
+    # FIXME: temporary setting
+    f[[3, 6], 0] = 1.001*np.ones(2)
+    # f[:, 1] = 0.1*np.ones(num_faces)
     f = f.flatten()
     node_coords_reshaped = S.node_coord.flatten()
-    idx = bnodes[-2:]
-    value = S.node_coord[idx, :]
+    # idx = bnodes[-2:]
+    # value = S.node_coord[idx, :]
     boundary_values = (idx, value)
     obj_args = {'f': f, 'gamma': gamma, 'boundary_values': boundary_values}
 
@@ -45,11 +51,16 @@ def test_linear_elasticity(setup_test):
                                       objfun=ela.obj_linear_elasticity)
     print(value)
     prb.set_obj_args(obj_args)
-    sol = prb.run(x0=node_coords_reshaped)
+    sol = prb.run(x0=node_coords_reshaped, maxeval=1000)
     sol_reshape = sol.reshape(S.node_coord.shape)
     print(prb.last_opt_result)
     print(sol.reshape(S.node_coord.shape))
-    print(S.node_coord)
+    epsilon = ela.get_strain(sol_reshape)
+    stress = ela.get_stress(epsilon)
+    print("ref_metric=", S.metric)
+    print("curr_metric=", S.get_current_metric_2D(sol.reshape(S.node_coord.shape)))
+    print("eps=", epsilon)
+    print("stress=", stress)
     np.save("ref.npy", S.node_coord)
     np.save("ela.npy", sol_reshape)
     np.save("conn.npy", S.S[2])