04dd84539a99b92eff8ebf9baac8bdd9b19e9338
Simone Manti <manti998@gmail.com>
2023-06-20 11:38:07 +0200

    Fixed and tested linear_elasticity.py

--- tests/test_linear_elasticity.py
+++ tests/test_linear_elasticity.py
@@ -24,33 +24,28 @@ def test_linear_elasticity(setup_test):
 
     mu_ = 1.
     lambda_ = 10.
+    # poisson's ratio
+    ni = lambda_/(2*(mu_ + lambda_))
     gamma = 1000.
     num_faces = S.S[2].shape[0]
     embedded_dim = S.embedded_dim
 
     # set displacement boundary conditions
-    # left_bnd_nodes_idx = [1, 3, 5]
-    # right_bnd_nodes_idx = [0, 2, 7]
-    # left_bnd_nodes_pos = S.node_coord[left_bnd_nodes_idx, :]
-    # right_bnd_nodes_pos = S.node_coord[right_bnd_nodes_idx, :]
-    # on the left edge, all the positions are blocked; on the right
-    # edge x-coordinate is multiplied by 1.1 while y-coordinate is fixed
-    # right_bnd_nodes_pos[:, 0] *= 1.1
-    # clamped_bnd_pos = np.vstack((left_bnd_nodes_pos, right_bnd_nodes_pos))
-    # clamped_bnd_idx = left_bnd_nodes_idx + right_bnd_nodes_idx
-    # boundary_values = (clamped_bnd_idx, clamped_bnd_pos)
     idx = [1, 3, 5]
     value = S.node_coord[idx, :]
     boundary_values = (idx, value)
 
     # set tractions boundary conditions
-    idx_tractions = jnp.array([0, 3, 6, 10])
-    bnd_tractions_values = jnp.zeros((4, 2), dtype=dt.float_dtype)
+    idx_tractions = jnp.array([0, 1, 3, 4, 6, 7, 9, 10])
+    bnd_tractions_values = jnp.zeros((8, 2), dtype=dt.float_dtype)
     boundary_tractions = (idx_tractions, bnd_tractions_values)
 
     ela = LinearElasticity(S=S, mu_=mu_, lambda_=lambda_)
     f = np.zeros((num_faces, (embedded_dim-1)))
-    f[[3, 6], 0] = 0.6*np.ones(2)
+    f[[0, 5], 1] = 0.25*np.ones(2)
+    f[[1, 7], 1] = -0.25*np.ones(2)
+    f[[2, 4], 0] = -0.4*np.ones(2)
+    f[[3, 6], 0] = 0.4*np.ones(2)
     f = f.flatten()
     node_coords_reshaped = S.node_coord.flatten()
 
@@ -62,19 +57,9 @@ def test_linear_elasticity(setup_test):
                                       objfun=ela.obj_linear_elasticity)
     prb.set_obj_args(obj_args)
     curr_node_coords = prb.run(x0=node_coords_reshaped)
-    print(prb.last_opt_result)
     curr_node_coords_reshape = curr_node_coords.reshape(S.node_coord.shape)
     node_coords_final_true = S.node_coord.copy()
     node_coords_final_true[:, 0] *= 1.1
-    epsilon = ela.get_strain(curr_node_coords_reshape)
-    stress = ela.get_stress(epsilon)
-    print("-------------------------------------")
-    print(ela.obj_linear_elasticity(node_coords_final_true,
-          f, gamma, boundary_values, boundary_tractions))
-    print(curr_node_coords_reshape)
-    print("-------------------------------------")
-    print("eps=", epsilon)
-    print("stress=", stress)
+    node_coords_final_true[:, 1] *= (1-0.1*ni)
     error = np.sum((node_coords_final_true - curr_node_coords_reshape)**2)
-    assert error < 1e-1
-    assert False
+    assert error < 1e-5
