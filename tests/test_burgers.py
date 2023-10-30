943ebde6d15fcd7bf06a8ee4e4b464f6f7a187ba
Simone Manti <manti998@gmail.com>
2023-10-30 09:10:59 +0100

    Fixed test_burgers for dimensionless burgers equation.

--- tests/test_burgers.py
+++ tests/test_burgers.py
@@ -4,43 +4,62 @@ from dctkit.physics import burgers as b
 
 
 def test_burgers(setup_test):
-    x_max = 5
-    t_max = 2
-    dx = 0.025
-    dt = 0.001
-    num_x_points = int(x_max/dx)
-    num_t_points = int(t_max/dt)
-    x = np.linspace(0, x_max, num_x_points)
-    x_circ = (x[:-1] + x[1:])/2
+    # SPACE PARAMS
+    L = 5
+    L_norm = 1
+    # spatial resolution
+    dx = 0.05
+    dx_norm = dx/L
+    #  Number of spatial grid points
+    num_x_points_norm = int(L_norm / dx_norm)
 
-    # boundary and initial conditions
-    u_0 = 2 * np.exp(-2 * (x_circ - 0.5 * x_max)**2)
-    nodes_BC = {'left': np.zeros(num_t_points), 'right': np.zeros(num_t_points)}
+    # vector containing spatial points
+    x_norm = np.linspace(0, L_norm, num_x_points_norm)
+    x_norm_circ = (x_norm[:-1] + x_norm[1:])/2
 
-    # viscosity coefficient
-    epsilon = 0.1
+    # initial velocity
+    u_0 = 2 * np.exp(-2 * (x_norm_circ - 0.5 * L)**2)
+    umax = np.max(u_0)
+
+    # TIME PARAMS
+    T = 2
+    T_norm = T*umax/L
+    # temporal resolution
+    dt = 0.01
+    dt_norm = dt*umax/L
+    # number of temporal grid points
+    num_t_points_norm = int(T_norm / dt_norm)
+
+    # Viscosity
+    epsilon = 0.06
+    epsilon_norm = epsilon/(L*umax)
+
+    nodes_BC = {'left': np.zeros(num_t_points_norm),
+                'right': np.zeros(num_t_points_norm)}
 
     # define u_FDM
     def FDM_run(u, epsilon, scheme="parabolic"):
-        for t in range(num_t_points - 1):
-            diffusion = epsilon * (u[2:, t] - 2 * u[1:-1, t] + u[:-2, t]) / dx**2
+        for t in range(num_t_points_norm - 1):
+            diffusion = epsilon * (u[2:, t] - 2 * u[1:-1, t] + u[:-2, t]) / dx_norm**2
             if scheme == "parabolic":
-                flux = (u[2:, t]**2 - u[:-2, t]**2) / (4*dx)
+                flux = (u[2:, t]**2 - u[:-2, t]**2) / (4*dx_norm)
             elif scheme == "upwind":
-                flux = (u[1:-1, t]**2 - u[:-2, t]**2) / (2*dx)
-            u[1:-1, t+1] = u[1:-1, t] + dt * (diffusion - flux)
+                flux = (u[1:-1, t]**2 - u[:-2, t]**2) / (2*dx_norm)
+            u[1:-1, t+1] = u[1:-1, t] + dt_norm * (diffusion - flux)
         return u
 
-    u_FDM_par = np.zeros([num_x_points-1, num_t_points], dtype=dt_.float_dtype)
-    u_FDM_par[:, 0] = u_0.copy()
+    u_FDM_par = np.zeros([num_x_points_norm-1, num_t_points_norm],
+                         dtype=dt_.float_dtype)
+    u_FDM_par[:, 0] = u_0.copy()/umax
     u_FDM_par[0, :] = nodes_BC['left']
     u_FDM_par[-1, :] = nodes_BC['right']
     u_FDM_up = u_FDM_par.copy()
-    u_FDM_par = FDM_run(u_FDM_par, epsilon, "parabolic")
+    u_FDM_par = FDM_run(u_FDM_par, epsilon_norm, "parabolic")
     u_FDM_up = FDM_run(u_FDM_up, 0, "upwind")
 
-    prb_par = b.Burgers(x_max, t_max, dx, dt, u_0, nodes_BC, epsilon)
-    prb_up = b.Burgers(x_max, t_max, dx, dt, u_0, nodes_BC, 0)
+    prb_par = b.Burgers(L_norm, T_norm, dx_norm, dt_norm,
+                        u_0/umax, nodes_BC, epsilon_norm)
+    prb_up = b.Burgers(L_norm, T_norm, dx_norm, dt_norm, u_0/umax, nodes_BC, 0)
     prb_par.run(scheme="parabolic")
     prb_up.run(scheme="upwind")
 
