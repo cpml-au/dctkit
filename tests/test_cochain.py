0dffe1c54fbf550b8289a1fd9b71ca67007e3743
Alessandro Lucantonio <alessandro.lucantonio@gmail.com>
2024-03-08 14:38:15 +0100

    Fixing linting.

--- tests/test_cochain.py
+++ tests/test_cochain.py
@@ -198,7 +198,7 @@ def test_hodge_star(setup_test):
                               71.11111111,  85.33333333,  95.31914894, 108.93617021,
                               153.2594235, 175.34246575, 196.00928074, 204.8,
                               220.39735099, 239.46547884],
-                              dtype=dctkit.float_dtype)[:, None]
+                             dtype=dctkit.float_dtype)[:, None]
     star_true_all = [star_cP0_true, star_cP1_true, star_cP2_true]
 
     for i in range(3):
