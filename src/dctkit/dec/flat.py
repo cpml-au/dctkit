db14f4749bff5f5d5ae6c88e06b9663e141f2957
Simone Manti <manti998@gmail.com>
2024-03-25 15:40:54 +0100

    Improvements.

--- src/dctkit/dec/flat.py
+++ src/dctkit/dec/flat.py
@@ -6,8 +6,8 @@ from typing import Callable, Dict, Optional
 
 
 def flat(c: C.CochainP0 | C.CochainD0, weights: Array, edges: C.CochainP1V |
-         C.CochainD1V, weighted_I: Optional[Callable] = None,
-         weighted_I_args: Optional[Dict] = {}) -> C.CochainP1 | C.CochainD1:
+         C.CochainD1V, interp_func: Optional[Callable] = None,
+         interp_func_args: Optional[Dict] = {}) -> C.CochainP1 | C.CochainD1:
     """Applies the flat to a vector/tensor-valued cochain representing a discrete
     vector/tensor field to obtain a scalar-valued cochain over primal/dual edges.
 
@@ -21,19 +21,19 @@ def flat(c: C.CochainP0 | C.CochainD0, weights: Array, edges: C.CochainP1V |
             interpolation scheme chosen for the input discrete vector/tensor field.
         edges: vector-valued cochain collecting the primal/dual edges over which the
             discrete vector/tensor field should be integrated.
-        weighted_I: interpolation function (callable) taking in input the cochain c
+        interp_func: interpolation function (callable) taking in input the cochain c
             and providing in output a 1-cochain of the same type (primal/dual). If
             it is None, then an interpolation function is built as W^T@c.coeffs.
-        weighted_I_args: additional keyword arguments for weighted_I
+        interp_func_args: additional keyword arguments for interp_func
     Returns:
         a primal/dual scalar/vector-valued cochain defined over primal/dual edges.
     """
-    if weighted_I is None:
+    if interp_func is None:
         # contract over the simplices of the input cochain (last axis of weights,
         # first axis of input cochain coeffs)
-        def weighted_I(x): return jnp.tensordot(weights.T, x.coeffs, axes=1)
-        weighted_I_args = {}
-    weighted_v = weighted_I(c, **weighted_I_args)
+        def interp_func(x): return jnp.tensordot(weights.T, x.coeffs, axes=1)
+        interp_func_args = {}
+    weighted_v = interp_func(c, **interp_func_args)
     # contract input vector/tensors with edge vectors (last indices of both
     # coefficient matrices), for each target primal/dual edge
     contract = partial(jnp.tensordot, axes=([-1,], [-1,]))
