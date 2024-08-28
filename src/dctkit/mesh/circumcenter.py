Fixed bug in circumcenter calculation due to new behavior of np.linalg.solve (numpy 2.0).

src/dctkit/mesh/circumcenter.py
@@ -7,8 +7,9 @@ from typing import Tuple
 # method of the SimplicialComplex class.
 
 
-def circumcenter(S: npt.NDArray,
-                 node_coords: npt.NDArray) -> Tuple[npt.NDArray, npt.NDArray]:
+def circumcenter(
+    S: npt.NDArray, node_coords: npt.NDArray
+) -> Tuple[npt.NDArray, npt.NDArray]:
     """Compute the circumcenter of a set of simplices (arranged row-wise).
         (Reference: Bell, Hirani, PyDEC: Software and Algorithms for Discretization
         of Exterior Calculus, 2012, Section 10.1).
@@ -36,7 +37,9 @@ def circumcenter(S: npt.NDArray,
     #        1_{1, m-1} |  0]
     # where m-1 is the number of nodes per simplex and B is the matrix (m-1)x(m-1)
     # defined as B_ij = 2 <v_i, v_j>
-    A = np.zeros((num_p, num_nodes_per_spx+1, num_nodes_per_spx+1), dtype=float_dtype)
+    A = np.zeros(
+        (num_p, num_nodes_per_spx + 1, num_nodes_per_spx + 1), dtype=float_dtype
+    )
     A[:, :num_nodes_per_spx, :num_nodes_per_spx] = 2 * S_coord @ transpose_S_coord
     A[:, :, -1] = 1
     A[:, -1, :] = 1
@@ -45,12 +48,15 @@ def circumcenter(S: npt.NDArray,
     # the vector of constant terms b for a given p-simplex s = {v_0,...,v_p} is
     # [<v_0,v_0>, ..., <v_p,v_p>, 1], where v_i is the coordinate of the
     # i-th node in s
-    b = np.ones((num_p, num_nodes_per_spx+1), dtype=float_dtype)
+    b = np.ones((num_p, num_nodes_per_spx + 1), dtype=float_dtype)
     b[:, range(num_nodes_per_spx)] = np.sum(S_coord * S_coord, axis=2)
 
     # barycentric coordinates x of the circumcenter are the solution
     # of the linear sistem Ax = b without the last component (i.e. x[:-1])
-    bary_coords_extended = np.linalg.solve(A, b)
+    # NOTE: reshaping b so that it is treated as a batch of column vectors is needed
+    # with numpy 2.0.
+    bary_coords_extended = np.linalg.solve(A, b[:, :, None])
+    bary_coords_extended = bary_coords_extended.reshape((num_p, num_nodes_per_spx + 1))
     bary_coords = bary_coords_extended[:, :-1]
 
     # the circumcenter of a p-simplex {v0,...,vp} can be written in barycentric

tests/test_circumcenter.py
@@ -12,11 +12,24 @@ def test_circumcenter(setup_test):
     S2 = mesh.cells_dict["triangle"]
     node_coords = mesh.points
 
-    circs_true = np.array([[0.5, 0, 0], [0, 0.5, 0], [1, 0.5, 0], [0.5, 1, 0]],
-                          dtype=dctkit.float_dtype)
-    bary_true = np.array([[0.5, 0.5, 0], [0.5, 0.5, 0], [0.5, 0.5, 0], [0.5, 0.5, 0]],
-                         dtype=dctkit.float_dtype)
+    circs_true = np.array(
+        [[0.5, 0, 0], [0, 0.5, 0], [1, 0.5, 0], [0.5, 1, 0]], dtype=dctkit.float_dtype
+    )
+    bary_true = np.array(
+        [[0.5, 0.5, 0], [0.5, 0.5, 0], [0.5, 0.5, 0], [0.5, 0.5, 0]],
+        dtype=dctkit.float_dtype,
+    )
     circs, bary = circ.circumcenter(S2, node_coords)
+    assert circs.dtype == dctkit.float_dtype
+    assert np.allclose(circs, circs_true)
+    assert np.allclose(bary, bary_true)
+
+    mesh, _ = util.generate_line_mesh(3)
+    S2 = mesh.cells_dict["line"]
+    node_coords = mesh.points
+    circs, bary = circ.circumcenter(S2, node_coords)
+    circs_true = np.array([[0.25, 0, 0], [0.75, 0.0, 0]], dtype=dctkit.float_dtype)
+    bary_true = np.array([[0.5, 0.5], [0.5, 0.5]])
     assert circs.dtype == dctkit.float_dtype
     assert np.allclose(circs, circs_true)
     assert np.allclose(bary, bary_true)